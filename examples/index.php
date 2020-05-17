be7a9ae315fefd57c1c445a47355de9ce37e1119
Valentino Pesce <valentino@iltuobrand.it>
2020-05-17 20:03:06 +0200

    Update index.php

--- examples/index.php
+++ examples/index.php
@@ -6,8 +6,10 @@ use Kenlog\Converter;
 
 // @constructor new Converter(string $xml, string $csv);
 $converter = new Converter('products.xml','products.csv');
+
 // @method setCSV(string $xpath, array $columns, string $separator = ',', int $skipLines = 0);
 $converter->setCSV('//Product',['Code', 'Description', 'Um', 'Qty']);
+
 // @var bool success
 if ($converter->success) {
     echo 'The XML file has been converted to CSV';
