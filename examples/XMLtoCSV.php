27e6be6286139645b396b776e93f7e4772158972
Valentino Pesce <valentino@iltuobrand.it>
2025-12-03 10:17:38 +0100

    Update XMLtoCSV.php

--- examples/XMLtoCSV.php
+++ examples/XMLtoCSV.php
@@ -1,17 +1,13 @@
-<?php 
+<?php
 
 require '../vendor/autoload.php';
 
 use Kenlog\Converter;
 
-// @constructor new Converter(string $xml);
 $converter = new Converter('products.xml');
+$converter->setCSV('products.csv', '//Product', ['Code', 'Description', 'Um', 'Qty']);
 
-// @method setCSV(string $csv, string $xpath, array $columns, string $separator = ',', int $skipLines = 0);
-$converter->setCSV('products.csv','//Product',['Code', 'Description', 'Um', 'Qty']);
-
-// @var bool success
-if ($converter->success) {
+if ($converter->isSuccess()) {
     echo 'The XML file has been converted to CSV';
 } else {
     echo 'The XML file was not converted to CSV';
