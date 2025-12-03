ca82cd2795a53edbbafe7bb0ae3a370a0c01b0e6
Valentino Pesce <valentino@iltuobrand.it>
2025-12-03 10:17:52 +0100

    Update XMLtoJSON.php

--- examples/XMLtoJSON.php
+++ examples/XMLtoJSON.php
@@ -1,17 +1,13 @@
-<?php 
+<?php
 
 require '../vendor/autoload.php';
 
 use Kenlog\Converter;
 
-// @constructor new Converter(string $xml);
 $converter = new Converter('products.xml');
-
-// @method setJSON(string $jsonFile);
 $converter->setJSON('products.json');
 
-// @var bool success
-if ($converter->success) {
+if ($converter->isSuccess()) {
     echo 'The XML file has been converted to JSON';
 } else {
     echo 'The XML file was not converted to JSON';
