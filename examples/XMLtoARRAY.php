49afe75c910a067b6e6a07e452f43a3f22bab627
Valentino Pesce <valentino@iltuobrand.it>
2025-12-03 10:17:20 +0100

    Update XMLtoARRAY.php

--- examples/XMLtoARRAY.php
+++ examples/XMLtoARRAY.php
@@ -1,19 +1,15 @@
-<?php 
+<?php
 
 require '../vendor/autoload.php';
 
 use Kenlog\Converter;
 
-// @constructor new Converter(string $xml);
 $converter = new Converter('products.xml');
-
-// @array getArray();
 $products = $converter->getArray();
 
-// The result will be an array containing all records
 foreach ($products['Product'] as $product) {
     echo $product['Code'] . PHP_EOL;
-    echo $product['Description']. PHP_EOL;
-    echo $product['Um']. PHP_EOL;
-    echo $product['Qty']. PHP_EOL;
+    echo $product['Description'] . PHP_EOL;
+    echo $product['Um'] . PHP_EOL;
+    echo $product['Qty'] . PHP_EOL;
 }
