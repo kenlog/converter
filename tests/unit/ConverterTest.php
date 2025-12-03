344c1fdb7ecffe94a083e95759c83b56eaa038b3
Valentino Pesce <valentino@iltuobrand.it>
2025-12-03 10:19:03 +0100

    Update ConverterTest.php

--- tests/unit/ConverterTest.php
+++ tests/unit/ConverterTest.php
@@ -9,28 +9,28 @@ class ConverterTest extends \Codeception\Test\Unit
     {
         $converter = new Converter('tests/_data/products.xml');
         $converter->setCSV('tests/_output/testConversionCSV1.csv', '//Product',['Code', 'Description', 'Um', 'Qty'],';');
-        $this->assertTrue($converter->success);
+        $this->assertTrue($converter->isSuccess());
     }
 
     public function testConversionCSV2()
     {
         $converter = new Converter('tests/_data/products.xml');
         $converter->setCSV('tests/_output/testConversionCSV2.csv','//Product',['Code','Qty']);
-        $this->assertTrue($converter->success);
+        $this->assertTrue($converter->isSuccess());
     }
 
     public function testConversionCSV3()
     {
         $converter = new Converter('tests/_data/products.xml');
         $converter->setCSV('tests/_output/testConversionCSV3.csv','//Product',['Code','Qty'],',',2);
-        $this->assertTrue($converter->success);
+        $this->assertTrue($converter->isSuccess());
     }
 
     public function testConversionJSON()
     {
         $converter = new Converter('tests/_data/products.xml');
         $converter->setJSON('tests/_output/testConversionJSON.json');
-        $this->assertTrue($converter->success);
+        $this->assertTrue($converter->isSuccess());
     }
 
     public function testConversionArray()
